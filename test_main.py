f0edac5aadc878aa6c8d636c90ec95aac173835d
jurrezwaan <jurrezwaan@gmail.com>
2022-11-08 12:08:13 +0100

    first commit

---
+++ test_main.py
@@ -0,0 +1,23 @@
+import pytest
+
+from main import app
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_index(client):
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, world!"
+
+
+def test_cat(client):
+    response = client.get("/cat")
+
+    assert response.status_code == 200
+    assert response.data == b"Miauw!"
