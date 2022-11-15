9bff603a9568bd9d7c1d96a81602ebc863399046
jurrezwaan <jurrezwaan@gmail.com>
2022-11-15 14:43:02 +0100

    run deploy

--- test_main.py
+++ test_main.py
@@ -13,7 +13,7 @@ def test_index(client):
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello, world!"
+    assert response.data == b"Hello, World!"
 
 
 def test_cat(client):
